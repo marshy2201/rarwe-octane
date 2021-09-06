c769f6d4cc39176cd1f29931e642cebb77e213b9
Lewis Marshall <marshy2201@yahoo.co.uk>
2021-09-06 11:55:05 +0100

    Chapter 5 Nested Routes

---
+++ app/routes/bands/band/songs.js
@@ -0,0 +1,9 @@
+import Route from '@ember/routing/route';
+
+export default class BandsBandSongsRoute extends Route {
+  model() {
+    const band = this.modelFor('bands.band');
+    
+    return band.songs;
+  }
+}
